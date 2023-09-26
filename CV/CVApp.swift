e1d25761a83bfe27034a37d176e05b4daf774b0e
buzruk <buzurgmexrs@gmail.com>
2023-09-26 16:40:33 +0500

    Initial Commit

---
+++ CV/CVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVApp.swift
+//  CV
+//
+//  Created by Buzurgmexr Sultonaliyev on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
